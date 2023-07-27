52844dad27e7e96c206dd6f58e12bdcf0b17f752
Leon van der Grient <leon@devtastic.build>
2023-07-27 22:05:32 +0200

    [core] feat: check user permissions for media library

--- packages/core/app/plugins/media-library/ui/MainMenuItem.tsx
+++ packages/core/app/plugins/media-library/ui/MainMenuItem.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import * as R from "ramda";
 
 import MainMenu from "~/ui/MainMenu";
+import useStrapi from "~/hooks/useStrapi";
 
 const MainMenuItem: React.FC = () => {
   const { t } = useTranslation();
+  const { permissions } = useStrapi();
+  const hasPermission = permissions.some(
+    R.whereEq({ action: "plugin::upload.read" })
+  );
 
-  return (
+  return hasPermission ? (
     <MainMenu.Item
       as={Link}
       to="/media-library"
       label={t("common.media_library")}
     />
-  );
+  ) : null;
 };
 
 export default MainMenuItem;
