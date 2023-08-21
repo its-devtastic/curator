296117973967543bb7f107192bc0eb68ddeacfb6
Leon van der Grient <leon@devtastic.build>
2023-08-21 11:12:26 +0200

    feat: add plausible to docs

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from "next/app";
+import PlausibleProvider from "next-plausible";
 
 import "./global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider domain="curatorjs.org">
+      <Component {...pageProps} />;
+    </PlausibleProvider>
+  );
 }
