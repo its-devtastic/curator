9c6ee9d6cfd3f74c8bd1a09b9e856292637f1307
Leon van der Grient <leon@devtastic.build>
2023-08-21 10:34:12 +0200

    fix: change asset prefix for documentation site

--- docs/next.config.js
+++ docs/next.config.js
@@ -6,5 +6,4 @@ const withNextra = require("nextra")({
 module.exports = withNextra({
   output: "export",
   images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === "production" ? "/curator" : "",
 });
